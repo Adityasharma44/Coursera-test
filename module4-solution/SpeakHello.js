95bfbd89d75a363febd02ac6437d040b01c72652
Aditya Sharma <as7007731@gmail.com>
2023-09-26 13:12:10 +0530

    added

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -8,11 +8,18 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
+let speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
+
+const helloSpeaker={
+  speak: function (name){
+    console.log(speakWord + " " + name);
+  }
+}
+
 function speak(name) {
   console.log(speakWord + " " + name);
 }
